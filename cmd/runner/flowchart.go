0ac848c72f2ab7432c048e4d3ada8ba96de3cd84
Damian Wilkolek <dwilkolek@protonmail.com>
2023-11-01 20:18:19 +0100

    Generate flowchart for single resource part2

--- cmd/runner/flowchart.go
+++ cmd/runner/flowchart.go
@@ -8,12 +8,13 @@ func GenerateFlowchart(resources []Resource, tag string) string {
 	flowchart := "flowchart TD\n"
 	for _, resource := range resources {
 		source := resource.Tag
-		if len(tag) != 0 && tag != source {
-			continue
-		}
+
 		for dep := range resource.References {
-			flowchart = flowchart + fmt.Sprintf("\t%s ---> %s\n", source, dep)
+			if len(tag) == 0 || (len(tag) > 0 && (source == tag || dep == tag)) {
+				flowchart = flowchart + fmt.Sprintf("\t%s ---> %s\n", source, dep)
+			}
 		}
 	}
+
 	return flowchart
 }
